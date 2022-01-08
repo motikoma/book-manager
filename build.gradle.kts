08ad5b0b729916253235fa05b542796a27635cf5
坂本圭佑 <>
2022-01-08 11:57:23 +0900

    add: bookControllerのユニットテスト追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.0")
--- src/test/kotlin/com/book/manager/controller/BookControllerTest.kt
+++ src/test/kotlin/com/book/manager/controller/BookControllerTest.kt
@@ -5,25 +5,28 @@ import com.book.manager.domain.model.Book
 import com.book.manager.domain.model.BookWithRental
 import com.book.manager.presentation.controller.BookController
 import com.book.manager.presentation.controller.BookInfo
+import com.book.manager.presentation.controller.GetBookDetailResponse
 import com.book.manager.presentation.controller.GetBookListResponse
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import java.nio.charset.StandardCharsets
 import java.time.LocalDate
+
 
 internal class BookControllerTest {
     private val bookService = mock<BookService>()
     private val bookController = BookController(bookService)
 
     @Test
-    fun `getList is success`(){
+    fun `getListが成功した場合、書籍情報のリストが返却される`(){
         val bookId = 100L
         val book = Book(bookId, "Kotlin", "太郎", LocalDate.now())
         val bookList = listOf(BookWithRental(book, null))
@@ -34,6 +37,22 @@ internal class BookControllerTest {
         val expected = ObjectMapper().registerKotlinModule().writeValueAsString(expectedResponse)
         val mockMVC = MockMvcBuilders.standaloneSetup(bookController).build()
         val resultResponse = mockMVC.perform(get("/book/list")).andExpect(status().isOk).andReturn().response
+        val result = resultResponse.getContentAsString(StandardCharsets.UTF_8)
+
+        assertThat(expected).isEqualTo(result)
+    }
+
+    @Test
+    fun `getDetailが成功した場合、bookIdに紐づく書籍情報が返却される`(){
+        val bookId = 100L
+        val book = Book(bookId, "Kotlin", "太郎", LocalDate.now())
+
+        whenever(bookService.getDetail(bookId)).thenReturn(BookWithRental(book, null))
+
+        val expectedResponse = GetBookDetailResponse(bookId, "Kotlin","太郎", LocalDate.now(), null)
+        val expected = ObjectMapper().registerModule(JavaTimeModule()).registerKotlinModule().writeValueAsString(expectedResponse)
+        val mockMVC = MockMvcBuilders.standaloneSetup(bookController).build()
+        val resultResponse = mockMVC.perform(get("/book/detail/$bookId")).andExpect(status().isOk).andReturn().response
         val result = resultResponse.getContentAsString(StandardCharsets.UTF_8)
 
         assertThat(expected).isEqualTo(result)
